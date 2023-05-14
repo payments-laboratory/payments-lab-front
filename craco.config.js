6727d2882dabfbd0152b37c7426dea3a18e86ede
jk <166354@gmail.com>
2023-05-14 22:23:18 +0900

    fix: config 수정

--- craco.config.js
+++ craco.config.js
@@ -4,13 +4,13 @@ const path = require("path");
 module.exports = {
     webpack: {
         alias: {
-            "@": path.resolve(__dirname, "src/"),
             "@components": path.resolve(__dirname, "src/common/components"),
             "@hooks": path.resolve(__dirname, "src/common/hooks"),
             "@libs": path.resolve(__dirname, "src/common/libs"),
             "@recoil": path.resolve(__dirname, "src/common/recoil"),
             "@router": path.resolve(__dirname, "src/common/router"),
             "@styles": path.resolve(__dirname, "src/common/styles"),
+            "@types": path.resolve(__dirname, "src/common/types"),
             "@pages": path.resolve(__dirname, "src/pages"),
             "@images": path.resolve(__dirname, "public/images"),
         },
