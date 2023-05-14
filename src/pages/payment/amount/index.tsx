25eb5dc4be9295a53ecfb538b7343e81dd508040
leaisrevolution <leaisrevolution@gmail.com>
2023-05-14 22:40:18 +0900

    Feat: Front-end test

--- src/pages/payment/amount/index.tsx
+++ src/pages/payment/amount/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PaymentAmountPage = () => {
     return (
         <div>
-            <h1>PaymentAmountPage</h1>
+            <h1>PaymentPage</h1>
         </div>
     );
 };
